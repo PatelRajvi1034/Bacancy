e9c2094f4b61d3fb75820021e8c8ba7fd33adb51
Your Name <you@example.com>
2025-03-20 14:18:06 +0530

    update

---
+++ EfCoreTask1/EfCoreTask1/Data/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace EfCoreTask1.Data
+{
+    public class Student
+    {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public string Course { get; set; }
+        
+    }
+}
+
---
+++ EfCoreTask1/EfCoreTask1/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿﻿using EfCoreTask1.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCoreTask1.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
